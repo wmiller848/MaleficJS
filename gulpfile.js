907e571cdb3f324d74eec5f6a879da7878d3f787
William Miller <williammiller@Williams-MacBook-Pro-2.local>
2014-11-09 19:39:42 -0600

    fixed build to handle errors better, gulp wish you helped more :p

--- gulpfile.js
+++ gulpfile.js
@@ -10,9 +10,8 @@ var coffeelint = require('gulp-coffeelint');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
-var jasmine = require('gulp-jasmine');
-var jasminePhantomJs = require('gulp-jasmine2-phantomjs');
-var jasminePh = require('gulp-jasmine-phantom');
+var jasmine = require('gulp-jasmine-phantom');
+var plumber = require('gulp-plumber');
 var del = require('del');
 
 var paths = {
@@ -20,53 +19,84 @@ var paths = {
   spec: ['spec/*.coffee']
 };
 
+var STATE_OK = 0,
+    STATE_ERR = 1;
+
+var state = STATE_OK;
+
+function handleError (err) {
+  console.log(err.toString());
+  this.emit('end');
+  state = STATE_ERR;
+  setTimeout(function() {
+    state = STATE_OK;
+  }, 50)
+}
+
 gulp.task('clean', function(cb) {
   del(['bin'], cb);
 });
 
 gulp.task('compile:dev', ['clean'], function() {
-  return gulp.src(paths.src)
-  	.pipe(sourcemaps.init())
-  	.pipe(coffeelint())
-    .pipe(coffeelint.reporter())
-  	.pipe(coffee())
-    .pipe(concat('malefic.coffee.js'))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('bin/dev'));
+  if (state === STATE_OK) {
+    return gulp.src(paths.src)
+    	.pipe(plumber(handleError))
+    	.pipe(sourcemaps.init())
+    	.pipe(coffeelint())
+      .pipe(coffeelint.reporter())
+    	.pipe(coffee())
+      .pipe(concat('malefic.coffee.js'))
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest('bin/dev'));
+  } else if (state === STATE_ERR) {
+    console.log('State is "error" skipping...');
+  }
 });
 
 gulp.task('compile:release', ['compile:dev'], function() {
-  return gulp.src(paths.src)
-  	.pipe(sourcemaps.init())
-  	.pipe(coffeelint())
-    .pipe(coffeelint.reporter())
-  	.pipe(coffee())
-    .pipe(uglify())
-    .pipe(concat('malefic.coffee.min.js'))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('bin/release'));
+  if (state === STATE_OK) {
+    return gulp.src(paths.src)
+    	.pipe(plumber(handleError))
+    	.pipe(sourcemaps.init())
+    	.pipe(coffeelint())
+      .pipe(coffeelint.reporter())
+    	.pipe(coffee())
+      .pipe(uglify())
+      .pipe(concat('malefic.coffee.min.js'))
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest('bin/release'));
+  } else if (state === STATE_ERR) {
+    console.log('State is "error" skipping...');
+  }
 });
 
 gulp.task('compile:spec', ['compile:release'], function() {
-	return gulp.src(paths.spec)
-		.pipe(coffeelint())
-    .pipe(coffeelint.reporter())
-  	.pipe(coffee())
-  	.pipe(concat('malefic.spec.coffee.js'))
-  	.pipe(gulp.dest('bin/spec'));
+  if (state === STATE_OK) {
+  	return gulp.src(paths.spec)
+  		.pipe(plumber(handleError))
+  		.pipe(coffeelint())
+      .pipe(coffeelint.reporter())
+    	.pipe(coffee())
+    	.pipe(concat('malefic.spec.coffee.js'))
+    	.pipe(gulp.dest('bin/spec'));
+  } else if (state === STATE_ERR) {
+    console.log('State is "error" skipping...');
+  }
 });
 
 gulp.task('spec', ['compile:spec'], function() {
-	//return gulp.src('spec/runner.html')
-  //.pipe(jasminePhantomJs());
-  return gulp.src(['bin/dev/malefic.coffee.js', 'bin/spec/malefic.spec.coffee.js'])
-  	.pipe(jasminePh({
-  		integration: true
-  	}));
+  if (state === STATE_OK) {
+    return gulp.src(['bin/dev/malefic.coffee.js', 'bin/spec/malefic.spec.coffee.js'])
+    	.pipe(jasmine({
+    		integration: true
+    	}));
+  } else if (state === STATE_ERR) {
+    console.log('State is "error" skipping...');
+  }
 });
 
 gulp.task('watch', function() {
-  gulp.watch('./src/lib/*.coffee', ['spec']);
+  gulp.watch('src/lib/*.coffee', ['spec']);
 });
 
-//gulp.task('default', ['watch']);
+gulp.task('default', ['spec']);
